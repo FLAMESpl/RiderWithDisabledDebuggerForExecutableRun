5c069c802dafefed1be9ce131c0d9e58f7537297
£ukasz Szafirski <lukasz.szafirski@ac-project.net>
2021-02-02 19:11:35 +0100

    Initial vommit

---
+++ GetSampleTextCommand.cs
@@ -0,0 +1,13 @@
+﻿using System.Management.Automation;
+
+namespace RiderWithDisabledDebuggerForExecutableRun
+{
+    [Cmdlet(VerbsCommon.Get, "SampleText")]
+    public class GetSampleTextCommand : Cmdlet
+    {
+        protected override void EndProcessing()
+        {
+            WriteObject("Hello world!");
+        }
+    }
+}
